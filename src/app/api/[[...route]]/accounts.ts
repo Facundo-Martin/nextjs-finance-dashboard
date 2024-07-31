af69865efeacba09b7bad077c5a1f728048ba9c7
Facundo Martin <facundomartin98@gmail.com>
2024-07-31 16:02:04 -0700

    chore: add tanstack query

--- src/app/api/[[...route]]/accounts.ts
+++ src/app/api/[[...route]]/accounts.ts
@@ -1,9 +1,13 @@
+import { db } from "@/db/drizzle";
+import { account } from "@/db/schema";
 import { Hono } from "hono";
 
-const app = new Hono().get("/", (c) => {
-  return c.json({
-    accounts: [],
-  });
+const app = new Hono().get("/", async (c) => {
+  const accounts = await db
+    .select({ id: account.id, name: account.name })
+    .from(account);
+
+  return c.json(accounts);
 });
 
 export default app;
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,14 +1,9 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import {
-  ClerkProvider,
-  SignInButton,
-  SignOutButton,
-  SignedIn,
-  SignedOut,
-  UserButton,
-} from "@clerk/nextjs";
+
+import { ClerkProvider } from "@clerk/nextjs";
+import { TanstackQueryProvider } from "@/providers/query-provider";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -24,9 +19,11 @@ export default function RootLayout({
 }>) {
   return (
     <ClerkProvider afterSignOutUrl="/">
-      <html lang="en">
-        <body className={inter.className}>{children}</body>
-      </html>
+      <TanstackQueryProvider>
+        <html lang="en">
+          <body className={inter.className}>{children}</body>
+        </html>
+      </TanstackQueryProvider>
     </ClerkProvider>
   );
 }
