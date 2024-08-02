ea73aebfa3480a0e4f95ca8d4ff68b26c2e9d7ed
Facundo Martin <facundomartin98@gmail.com>
2024-08-02 10:08:09 -0700

    feat: create first data fetching hook/function

--- src/app/(dashboard)/page.tsx
+++ src/app/(dashboard)/page.tsx
@@ -1,3 +1,20 @@
+"use client";
+
+import { useGetAccounts } from "@/features/accounts/api/use-get-accounts";
+
 export default function DashboardPage() {
-  return <div>Dashboard page</div>;
+  const { data: accounts, isLoading } = useGetAccounts();
+
+  if (isLoading) return "Loading";
+
+  return (
+    <div>
+      <h1>Dashboard page</h1>
+      <div>
+        {accounts?.map((account) => (
+          <div key={account.id}>{account.name}</div>
+        ))}
+      </div>
+    </div>
+  );
 }
