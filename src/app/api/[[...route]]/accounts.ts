8d62a91befbbbd7da0d27a670b2cb4acbab6c9d9
Facundo Martin <facundomartin98@gmail.com>
2024-08-02 10:07:56 -0700

    chore: config hono rpc

--- src/app/api/[[...route]]/accounts.ts
+++ src/app/api/[[...route]]/accounts.ts
@@ -1,13 +1,23 @@
 import { db } from "@/db/drizzle";
 import { account } from "@/db/schema";
 import { Hono } from "hono";
+import { clerkMiddleware, getAuth } from "@hono/clerk-auth";
+import { HTTPException } from "hono/http-exception";
 
-const app = new Hono().get("/", async (c) => {
-  const accounts = await db
+const app = new Hono().get("/", clerkMiddleware(), async (c) => {
+  const auth = getAuth(c);
+
+  if (!auth?.userId) {
+    throw new HTTPException(401, {
+      res: c.json({ error: "Unauthorized" }, 401),
+    });
+  }
+
+  const data = await db
     .select({ id: account.id, name: account.name })
     .from(account);
 
-  return c.json(accounts);
+  return c.json({ data });
 });
 
 export default app;
