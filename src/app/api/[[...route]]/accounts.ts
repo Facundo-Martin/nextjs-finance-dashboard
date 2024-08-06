4ebb3113b0471c70c9c648e33f5f1d819f324e6e
Facundo Martin <facundomartin98@gmail.com>
2024-08-05 17:49:30 -0700

    chore: write bulk delete endpoint

--- src/app/api/[[...route]]/accounts.ts
+++ src/app/api/[[...route]]/accounts.ts
@@ -2,10 +2,10 @@ import { db } from "@/db/drizzle";
 import { account, insertAccountSchema } from "@/db/schema";
 import { Hono } from "hono";
 import { clerkMiddleware, getAuth } from "@hono/clerk-auth";
-import { HTTPException } from "hono/http-exception";
-import { eq } from "drizzle-orm";
+import { and, eq, inArray } from "drizzle-orm";
 import { zValidator } from "@hono/zod-validator";
 import { createId } from "@paralleldrive/cuid2";
+import { z } from "zod";
 
 const app = new Hono()
   .get("/", clerkMiddleware(), async (c) => {
@@ -44,6 +44,36 @@ const app = new Hono()
           name: values.name,
         })
         .returning();
+
+      return c.json({ data });
+    }
+  )
+  .post(
+    "/bulk-delete",
+    clerkMiddleware(),
+    zValidator(
+      "json",
+      z.object({
+        ids: z.array(z.string()),
+      })
+    ),
+    async (c) => {
+      const auth = getAuth(c);
+
+      if (!auth?.userId) {
+        return c.json({ error: "Unauthorized" }, 401);
+      }
+
+      const values = c.req.valid("json");
+
+      const data = await db
+        .delete(account)
+        .where(
+          and(eq(account.userId, auth.userId), inArray(account.id, values.ids))
+        )
+        .returning({
+          id: account.id,
+        });
 
       return c.json({ data });
     }
