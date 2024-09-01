06ffc634d30cf810dff937027121a5ae0b450b35
Facundo Martin <facundomartin98@gmail.com>
2024-09-01 11:35:48 -0400

    feat: add confirmation dialog on bulk delete

--- src/components/data-table.tsx
+++ src/components/data-table.tsx
@@ -26,6 +26,7 @@ import {
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
 import { Trash } from "lucide-react";
+import { useConfirm } from "@/hooks/use-confirm";
 
 interface DataTableProps<TData, TValue> {
   columns: ColumnDef<TData, TValue>[];
@@ -48,6 +49,11 @@ export function DataTable<TData, TValue>({
   );
   const [rowSelection, setRowSelection] = React.useState({});
 
+  const [ConfirmDialog, confirm] = useConfirm(
+    "Are you sure?",
+    "You are about to perform a bulk delete"
+  );
+
   const table = useReactTable({
     data,
     columns,
@@ -67,6 +73,7 @@ export function DataTable<TData, TValue>({
 
   return (
     <div>
+      <ConfirmDialog />
       <div className="flex items-center py-4">
         <Input
           placeholder={`Filter ${filterKey}s...`}
@@ -81,9 +88,13 @@ export function DataTable<TData, TValue>({
             size="sm"
             variant="outline"
             className="ml-auto font-normal text-xs"
-            onClick={() => {
-              onDelete(table.getFilteredSelectedRowModel().rows);
-              table.resetRowSelection();
+            onClick={async () => {
+              const ok = await confirm();
+
+              if (ok) {
+                onDelete(table.getFilteredSelectedRowModel().rows);
+                table.resetRowSelection();
+              }
             }}
             disabled={disabled}
           >
---
+++ src/hooks/use-confirm.tsx
@@ -0,0 +1,62 @@
+import { Button } from "@/components/ui/button";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import { useState } from "react";
+
+export function useConfirm(
+  title: string,
+  message: string
+): [() => JSX.Element, () => Promise<unknown>] {
+  const [promise, setPromise] = useState<{
+    resolve: (value: boolean) => void;
+  } | null>(null);
+
+  function confirm() {
+    return new Promise((resolve, reject) => {
+      setPromise({ resolve });
+    });
+  }
+
+  function handleClose() {
+    setPromise(null);
+  }
+
+  function handleConfirm() {
+    promise?.resolve(true);
+    handleClose();
+  }
+
+  function handleCancel() {
+    promise?.resolve(false);
+    handleClose();
+  }
+
+  function ConfirmationDialog() {
+    return (
+      <Dialog open={!!promise}>
+        <DialogTrigger>Open</DialogTrigger>
+        <DialogContent>
+          <DialogHeader>
+            <DialogTitle>{title}</DialogTitle>
+            <DialogDescription>{message}</DialogDescription>
+          </DialogHeader>
+          <DialogFooter className="pt-2">
+            <Button onClick={handleCancel} variant="outline">
+              Cancel
+            </Button>
+            <Button onClick={handleConfirm}>Confirm</Button>
+          </DialogFooter>
+        </DialogContent>
+      </Dialog>
+    );
+  }
+
+  return [ConfirmationDialog, confirm];
+}
