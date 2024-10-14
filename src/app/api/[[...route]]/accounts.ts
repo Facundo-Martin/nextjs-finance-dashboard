67d6172094151b6cfd1750517ba4d5644070e305
Facundo Martin <facundomartin98@gmail.com>
2024-10-14 12:08:17 -0300

    chore: create get and patch account endpoints

--- src/app/api/[[...route]]/accounts.ts
+++ src/app/api/[[...route]]/accounts.ts
@@ -22,6 +22,39 @@ const app = new Hono()
 
     return c.json({ data });
   })
+  .get(
+    "/:id",
+    zValidator(
+      "param",
+      z.object({
+        id: z.string().optional(),
+      })
+    ),
+    clerkMiddleware(),
+    async (c) => {
+      const auth = getAuth(c);
+      const { id } = c.req.valid("param");
+
+      if (!id) {
+        return c.json({ error: "Missing id" }, 400);
+      }
+
+      if (!auth?.userId) {
+        return c.json({ error: "Unauthorized" }, 401);
+      }
+
+      const [data] = await db
+        .select({ id: account.id, name: account.name })
+        .from(account)
+        .where(and(eq(account.userId, auth.userId), eq(account.id, id)));
+
+      if (!data) {
+        return c.json({ error: "Not found" }, 404);
+      }
+
+      return c.json({ data });
+    }
+  )
   .post(
     "/",
     clerkMiddleware(),
@@ -74,6 +107,47 @@ const app = new Hono()
         .returning({
           id: account.id,
         });
+
+      return c.json({ data });
+    }
+  )
+  .patch(
+    "/:id",
+    clerkMiddleware(),
+    zValidator(
+      "param",
+      z.object({
+        id: z.string().optional(),
+      })
+    ),
+    zValidator(
+      "json",
+      insertAccountSchema.pick({
+        name: true,
+      })
+    ),
+    async (c) => {
+      const auth = getAuth(c);
+      const { id } = c.req.valid("param");
+      const values = c.req.valid("json");
+
+      if (!id) {
+        return c.json({ error: "Missing id" }, 400);
+      }
+
+      if (!auth?.userId) {
+        return c.json({ error: "Unauthorized" }, 401);
+      }
+
+      const [data] = await db
+        .update(account)
+        .set(values)
+        .where(and(eq(account.id, id), eq(account.userId, auth.userId)))
+        .returning();
+
+      if (!data) {
+        return c.json({ error: "Not found" }, 400);
+      }
 
       return c.json({ data });
     }
