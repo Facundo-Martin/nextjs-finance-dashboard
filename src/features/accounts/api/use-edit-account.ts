fc44f6160813dad450ba17e007661cfc553a3bdc
Facundo Martin <facundomartin98@gmail.com>
2024-10-14 12:09:51 -0300

    chore: integrate BE endpoints on the FE with custom hooks (w/ tanstack query)

---
+++ src/features/accounts/api/use-edit-account.ts
@@ -0,0 +1,36 @@
+import { client } from "@/lib/hono";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { InferRequestType, InferResponseType } from "hono";
+import { toast } from "sonner";
+
+type ResponseType = InferResponseType<
+  (typeof client.api.accounts)[":id"]["$patch"]
+>;
+type RequestType = InferRequestType<
+  (typeof client.api.accounts)[":id"]["$patch"]
+>["json"];
+
+export function useEditAccount(id?: string) {
+  const queryClient = useQueryClient();
+
+  const mutation = useMutation<ResponseType, Error, RequestType>({
+    mutationFn: async (json) => {
+      const response = await client.api.accounts[":id"]["$patch"]({
+        json,
+        param: { id },
+      });
+      return await response.json();
+    },
+    onSuccess: () => {
+      toast.success("Account updated");
+      queryClient.invalidateQueries({ queryKey: ["accounts"] });
+      queryClient.invalidateQueries({ queryKey: ["account", { id }] });
+      // TODO: Invalidate account summary and transactions
+    },
+    onError: () => {
+      toast.error("Failed to create account");
+    },
+  });
+
+  return mutation;
+}
